668948967f0c579116957bea054a96a9a4555015
GAUTAMBISOI <gautambisoi7906@gmail.com>
2025-06-07 20:29:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+
+const fs=require('fs');
+const express=require('express');
+const path = require('path');
+const app=express();
+const port=3001;
+
+app.set("view engine","ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res,next)=>{
+   fs.readdir(`./files`,function(err,files){
+     res.render("index",{files:files});  
+   })
+   })
+ app.get("/file/:filename",(req,res,next)=>{
+   fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+      res.render("show", {filename:req.params.filename, filedata:filedata});
+      
+   })
+   })
+
+   app.get("/edit/:filename",(req,res,next)=>{
+      res.render("edit",{filename:req.params.filename});
+     
+   }
+   )
+   
+  
+   app.post("/edit",(req,res,next)=>{  
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+   
+         console.log("name changed succesfully");
+         
+          res.redirect("/");
+      
+    })
+   }) 
+   
+app.post("/check",(req,res,next)=>{
+   fs.writeFile(`./files/${req.body.task.split(" ").join('')}.txt`,req.body.detail,function(err){
+      res.redirect('/');
+   })
+ })
+
+  
+
+
+
+app.listen(port);
